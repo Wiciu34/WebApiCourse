ecdfca8b28a4b7c48efaf6db6a4c1a59d626c1fc
Wiktor L <wiktor.langier7@gmail.com>
2024-07-17 14:41:37 +0200

    add commentDto and Comment mapper

---
+++ api/Mappers/CommentMappers.cs
@@ -0,0 +1,19 @@
+using api.Dtos.Comment;
+using api.Models;
+
+namespace api.Mappers;
+
+public static class CommentMappers
+{
+    public static CommentDto ToCommentDto(this Comment comment)
+    {
+        return new CommentDto
+        {
+            Id = comment.Id,
+            Title = comment.Title,
+            Content = comment.Content,
+            CreatedOn = comment.CreatedOn,
+            StockId = comment.StockId,
+        };
+    }
+}
