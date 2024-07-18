8afd88b160880b26d3703d4bd8d3d992f369076d
Wiktor L <wiktor.langier7@gmail.com>
2024-07-18 16:04:58 +0200

    Add sorting by stock symbol

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+caa7c79007652e7a03715a8f9077c8a35ad3c225")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64dacc406f857b9ac034f49cd30f4a2aa181f4cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
