9dd13cccfca0cfc514e086fb955cd3c50e79d6a7
Wiktor L <wiktor.langier7@gmail.com>
2024-08-19 10:24:30 +0200

    Modify card List component to render cards from with data from search Result

--- frontend/src/Components/Card/Card.tsx
+++ frontend/src/Components/Card/Card.tsx
@@ -1,22 +1,22 @@
 import React from "react";
 import "./Card.css"
+import { CompanySearch } from "../../company";
 
 interface Props {
-    companyName: string;
-    tricker: string;
-    price: number;
+    id: string;
+    searchResult: CompanySearch;
 }
 
-const Card: React.FC<Props>  = ({companyName, tricker, price}) : JSX.Element => {
+const Card: React.FC<Props>  = ({id, searchResult}) : JSX.Element => {
   return (
      <div className="card">
         <img src="https://pobierak.jeja.pl/images/4/a/5/181296_to-uczucie-kiedy-twoje-obrazki-i.jpg" alt='Image' />
         <div className='details'>
-            <h2>{companyName} ({tricker})</h2>
-            <p>${price}</p>
+            <h2>{searchResult.name} ({searchResult.symbol})</h2>
+            <p>{searchResult.currency}</p>
         </div>
         <p className='info'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, laboriosam?
+            {searchResult.exchangeShortName} - {searchResult.stockExchange}
         </p>
      </div>
   );
