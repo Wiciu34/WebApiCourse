a8b771142f6c813ae76930807e9432ccd841563e
Wiktor L <wiktor.langier7@gmail.com>
2024-08-19 10:04:55 +0200

    Add conditional rendering to display message when error occurs

--- frontend/src/Components/Search/Search.tsx
+++ frontend/src/Components/Search/Search.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useState, SyntheticEvent, FormEvent } from 'react'
+import React, { ChangeEvent, SyntheticEvent } from 'react'
 
 interface Props {
   onClick: (e: SyntheticEvent) => void;
