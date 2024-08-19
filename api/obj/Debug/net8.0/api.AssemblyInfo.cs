83d0120aafd8f263c5947fe0303ee2a4aedd0bda
Wiktor L <wiktor.langier7@gmail.com>
2024-08-19 08:46:36 +0200

    Add api.tsx and company.d.ts to handle calls from FMP api

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+632e49019805b5b4ca103233e4efc50c6cbc0a87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9794c3888edfee69dcfc57158ae56e5153c7f5ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
