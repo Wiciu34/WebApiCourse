3d607602b8f379a8315d18980557f481a952f879
Wiktor L <wiktor.langier7@gmail.com>
2024-08-19 09:01:50 +0200

    Refactor Search component by moving logic to app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,25 @@
-import './App.css';
-import CardList from './Components/CardList/CardList';
-import Search from './Components/Search/Search';
+import { ChangeEvent, SyntheticEvent, useState } from "react";
+import "./App.css";
+import CardList from "./Components/CardList/CardList";
+import Search from "./Components/Search/Search";
 
 function App() {
-  return (
-    <div className="App">
-      <Search />
-      <CardList/>
-    </div>
-  );
+   const [search, setSearch] = useState<string>("");
+
+   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+      setSearch(e.target.value);
+      console.log(e);
+   };
+
+   const onClick = (e: SyntheticEvent) => {
+      console.log(e);
+   };
+   return (
+      <div className="App">
+         <Search onClick={onClick} search={search} handleChange={handleChange}/>
+         <CardList />
+      </div>
+   );
 }
 
 export default App;
