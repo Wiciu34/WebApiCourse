d720ce4d7da7e7e450ed0aa8ce1aa61cafa0a680
Wiktor L <wiktor.langier7@gmail.com>
2024-08-22 11:24:36 +0200

    Create companyDashbord and sidebar components and add its to CompanyPage

--- frontend/src/Pages/CompanyPage/CompanyPage.tsx
+++ frontend/src/Pages/CompanyPage/CompanyPage.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router"
 import { CompanyProfile } from "../../company";
 import { getCompanyProfile } from "../../api";
+import Sidebar from "../../Components/Sidebar/Sidebar";
+import CompanyDashboard from "../../Components/CompanyDashboard/CompanyDashboard";
 
 interface Props {}
 
@@ -18,14 +20,17 @@ const CompanyPage = (props: Props) => {
     getProfileInit();
   }, [])
   return (
-    <>
-       {company ? (
-        <div>{company.companyName}</div>
-       ) : (
-        <div>Company not found!</div>
-       )} 
-    </>
-  )
+     <>
+        {company ? (
+           <div className="w-full relative flex ct-docs-disable-sidebar-content overflow-x-hidden">
+            <Sidebar/>
+            <CompanyDashboard/> 
+           </div>
+        ) : (
+           <div>Company not found!</div>
+        )}
+     </>
+  );
 }
 
 export default CompanyPage
