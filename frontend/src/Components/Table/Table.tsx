9121d369aff3f61c2aac91ffd32ed505a1ede7bf
Wiktor L <wiktor.langier7@gmail.com>
2024-08-23 11:32:29 +0200

    Add logic to the table component and the table to design page

--- frontend/src/Components/Table/Table.tsx
+++ frontend/src/Components/Table/Table.tsx
@@ -1,11 +1,60 @@
-import React from 'react'
+import { testIncomeStatementData } from './tableTestData';
 
 type Props = {}
 
+const data = testIncomeStatementData;
+
+type Company = (typeof data)[0]
+
+const configs = [
+   {
+      label: "Year",
+      render: (company: Company) => {
+         return company.acceptedDate;
+      },
+   },
+   {
+      label: "Cost of revenue",
+      render: (company: Company) => {
+         return company.costOfRevenue;
+      },
+   }
+];
+
 const Table = (props: Props) => {
+    const renderedRows = data.map(company => {
+        return (
+            <tr key={company.cik}>
+                {configs.map((value: any)=>{
+                    return (
+                        <td className="p-4 whitespace-nowrap text-sm font-normal text-gray-900">{value.render(company)}</td>
+                    )
+                })}
+            </tr>
+        )
+    })
+
+    const renderedHeaders = configs.map(config => {
+        return (
+           <th
+              className="p-4 text-left text-xs font-medium text-fray-500 uppercase tracking-wider"
+              key={config.label}
+           >
+              {config.label}
+           </th>
+        );
+    })
+        
   return (
-    <div>Table</div>
-  )
+     <div className="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8">
+        <table>
+           <thead className="min-w-full divide-y divide=gray-200 m-5">
+              <tr>{renderedHeaders}</tr>
+           </thead>
+           <tbody>{renderedRows}</tbody>
+        </table>
+     </div>
+  );
 }
 
 export default Table
--- frontend/src/Pages/DesignPage/DesignPage.tsx
+++ frontend/src/Pages/DesignPage/DesignPage.tsx
@@ -1,10 +1,14 @@
+import Table from "../../Components/Table/Table";
 
 type Props = {}
 
 const DesignPage = (props: Props) => {
   return (
-    <div>This is design page</div>
-  )
+     <>
+        <div>This is design page</div>
+        <Table/>
+     </>
+  );
 }
 
 export default DesignPage
