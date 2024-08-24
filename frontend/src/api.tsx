dc9b51eadf5568ed1551c432ecae79e89fa9cbd8
Wiktor L <wiktor.langier7@gmail.com>
2024-08-24 18:37:18 +0200

    Add logic to balance sheet component and correct mistake in getBalanceSheet method

--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -58,10 +58,10 @@ export const getIncomeStatement = async (query: string) => {
    }
 };
 
-export const getBalancdeSheet = async (query: string) => {
+export const getBalanceSheet = async (query: string) => {
    try {
       const data = await axios.get<CompanyBalanceSheet[]>(
-         `https://financialmodelingprep.com/api/v3/balance-sheet/${query}?limit=40&apikey=${process.env.REACT_APP_API_KEY}`
+         `https://financialmodelingprep.com/api/v3/balance-sheet-statement/${query}?limit=40&apikey=${process.env.REACT_APP_API_KEY}`
       );
       return data;
    } catch (error: any) {
