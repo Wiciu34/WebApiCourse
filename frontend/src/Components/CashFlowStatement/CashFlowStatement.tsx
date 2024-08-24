2f1723c4f261cea337496c7f12fba60aa14d7bda
Wiktor L <wiktor.langier7@gmail.com>
2024-08-24 19:27:00 +0200

    Create CashFlowStatement component and add link to it in sidebar

---
+++ frontend/src/Components/CashFlowStatement/CashFlowStatement.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const CashFlowStatement = (props: Props) => {
+  return (
+    <div>CashFlowStatement</div>
+  )
+}
+
+export default CashFlowStatement
