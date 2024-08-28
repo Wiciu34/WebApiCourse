5886e50a867b192888ec0a7f006a4a63529661d8
Wiktor L <wiktor.langier7@gmail.com>
2024-08-28 12:26:40 +0200

    Add registerApi method

--- frontend/src/Services/AuthService.ts
+++ frontend/src/Services/AuthService.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { UserProfile } from "../Models/User";
+import { handleError } from "../Helpers/ErrorHandler";
 
 const api =  "http://localhost:5080/api";
 
@@ -12,6 +13,20 @@ export const loginApi = async(username: string, password: string) => {
         return data;
     }
     catch(error){
+        handleError(error)
+    }
+}
 
+export const registerApi = async(username: string, password: string, email: string) => {
+    try {
+        const data = await axios.post<UserProfile>(api + "account/Register", {
+            username: username,
+            password: password,
+            email: email
+        })
+        return data;
+    }
+    catch(error){
+        handleError(error)
     }
 }
